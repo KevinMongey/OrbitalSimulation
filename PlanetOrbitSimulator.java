e311daaaaa8d0298e4caa918a09306dd7f349869
Kevin Mongey <78176147+KevinMongey@users.noreply.github.com>
2025-03-20 12:33:06 +0000

    Update PlanetOrbitSimulator.java

--- PlanetOrbitSimulator.java
+++ PlanetOrbitSimulator.java
@@ -1,8 +1,6 @@
 import java.awt.*;
 import javax.swing.*;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         //sets up window
